edd4dbf56bd38ee23e5e6b0cfe8157e718f32327
yaroslav.zozulia <yaroslav.zozulia@devit.group>
2025-04-12 13:21:21 +0300

    - update typography

--- src/components/common/Typography/Typography.tsx
+++ src/components/common/Typography/Typography.tsx
@@ -1,8 +1,9 @@
 import { FC, JSX, ReactNode } from 'react';
+import classNames from 'classnames';
 
 import s from './styles.module.scss';
 
-type Variant = 'h1' | 'h2' | 'h3' | 'body1' | 'body2' | 'decorSpan';
+type Variant = 'h1' | 'h2' | 'h3' | 'body1' | 'body2' | 'p' | 'decorSpan';
 
 interface TypographyProps {
   variant: Variant;
@@ -10,15 +11,29 @@ interface TypographyProps {
   className?: string;
 }
 
+const mergeClassNames = (
+  moduleClass: string | string[] | undefined,
+  className?: string,
+  options?: {
+    classNameFirst?: boolean; // если true — ставит className перед moduleClass
+  }
+): string => {
+  const mod = Array.isArray(moduleClass) ? moduleClass : [moduleClass];
+
+  return options?.classNameFirst
+    ? classNames(className, ...mod)
+    : classNames(...mod, className);
+};
+
 const tagMap: Record<Variant, keyof JSX.IntrinsicElements> = {
   h1: 'h1',
   h2: 'h2',
   h3: 'h3',
+  p: 'p',
   body1: 'p',
   body2: 'p',
   decorSpan: 'span'
 };
-
 export const Typography: FC<TypographyProps> = ({
   variant,
   children,
@@ -26,5 +41,13 @@ export const Typography: FC<TypographyProps> = ({
 }) => {
   const Tag = tagMap[variant];
 
-  return <Tag className={`${s[variant]} ${className || ''}`}>{children}</Tag>;
+  return (
+    <Tag
+      className={mergeClassNames(s[variant], className, {
+        classNameFirst: false
+      })}
+    >
+      {children}
+    </Tag>
+  );
 };
--- src/components/pages/home/Advantages/Advantages.tsx
+++ src/components/pages/home/Advantages/Advantages.tsx
@@ -9,28 +9,26 @@ export const AdvantagesPage = () => {
       <AppContainer>
         <ul className={s.list}>
           <li className={s.listItem}>
-            <Typography variant={'body2'} className={s.listItemTitle}>
-              431<span className={s.listItemTitleSpan}>M.€</span>
+            <Typography variant={'p'} className={s.listItemTitle}>
+              431 <span className={s.listItemTitleSpan}>M.€</span>
             </Typography>
-            <Typography variant={'body2'} className={s.listItemText}>
+            <Typography variant={'p'} className={s.listItemText}>
               Jährlicher Ertragsverlust durch verschmutzte PV-Anlagen
             </Typography>
           </li>
           <li className={s.listItem}>
-            <Typography variant={'body2'} className={s.listItemTitle}>
-              12,2
-              <span className={s.listItemTitleSpan}>%</span>
+            <Typography variant={'p'} className={s.listItemTitle}>
+              12,2 <span className={s.listItemTitleSpan}>%</span>
             </Typography>
-            <Typography variant={'body2'} className={s.listItemText}>
+            <Typography variant={'p'} className={s.listItemText}>
               Anteil des PV erzeugten Stroms an der Gesamterzeugung
             </Typography>
           </li>
           <li className={s.listItem}>
-            <Typography variant={'body2'} className={s.listItemTitle}>
-              81,2
-              <span className={s.listItemTitleSpan}>M.kW</span>
+            <Typography variant={'p'} className={s.listItemTitle}>
+              81,2 <span className={s.listItemTitleSpan}>M.kW</span>
             </Typography>
-            <Typography variant={'body2'} className={s.listItemText}>
+            <Typography variant={'p'} className={s.listItemText}>
               Installierte PV Leistung in Deutschland
             </Typography>
           </li>
