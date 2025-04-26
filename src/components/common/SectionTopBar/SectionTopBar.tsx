796f223702934e345c4bc6808998abe9b4e3500f
yaroslav.zozulia <yaroslav.zozulia@devit.group>
2025-04-26 14:58:58 +0300

    - add CleaningSection - add common component SectionTopBar (decor line on section top)

---
+++ src/components/common/SectionTopBar/SectionTopBar.tsx
@@ -0,0 +1,5 @@
+import s from './styles.module.scss';
+
+export const SectionTopBar = () => {
+  return <div className={s.sectionTopBar}> </div>;
+};
--- src/components/pages/home/CleaningSection/CleaningSection.tsx
+++ src/components/pages/home/CleaningSection/CleaningSection.tsx
@@ -1,16 +1,36 @@
+import Image from 'next/image';
+
 import { AppContainer } from '@/components/common/AppContainer/AppContainer';
 import { Typography } from '@/components/common/Typography/Typography';
 
 import s from './styles.module.scss';
+import { SectionTopBar } from '@/components/common/SectionTopBar/SectionTopBar';
 
 export const CleaningSection = () => {
   return (
-    <section className={s.clearAllSection}>
-      <AppContainer>
-        <Typography variant={'h2'} className={s.sectionTitle}>
-          <Typography variant={'decorSpan'}>Warum</Typography> ist die Reinigung
-          von PV-Systemen wichtig?
-        </Typography>
+    <section className={s.cleaningSection}>
+      <SectionTopBar />
+      <AppContainer classes={s.cleaningSectionContainer}>
+        <div>
+          <Typography variant={'h2'} className={s.sectionTitle}>
+            <Typography variant={'decorSpan'}>Warum</Typography> ist die
+            Reinigung von PV-Systemen wichtig?
+          </Typography>
+          <Typography variant={'body1'} className={s.text}>
+            Verschmutzte Solarpaneele verlieren bis zu 30 % ihrer Leistung, da
+            sie weniger Sonnenlicht absorbieren. Staub, Laub und Ablagerungen
+            beeinträchtigen die Effizienz und können langfristige Schäden
+            verursachen. Eine regelmäßige Reinigung sorgt für maximale
+            Energieausbeute und verlängert die Lebensdauer Ihrer Anlage.
+          </Typography>
+        </div>
+        <Image
+          src={'/img/home/cleaningSection/animation.gif'}
+          width={665}
+          height={558}
+          alt={'animation'}
+          className={s.animation}
+        />
       </AppContainer>
     </section>
   );
