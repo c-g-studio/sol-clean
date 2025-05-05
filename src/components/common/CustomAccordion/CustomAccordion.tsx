e531d985c85a2951a8ab147ab8912e54afcbdc66
yaroslav.zozulia <yaroslav.zozulia@devit.group>
2025-05-05 20:40:55 +0300

    - add reuseable component CustomAccordion

---
+++ src/components/common/CustomAccordion/CustomAccordion.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { FC } from 'react';
+import { Accordion, AccordionItem } from '@heroui/accordion';
+
+import { Typography } from '@/components/common/Typography/Typography';
+import { Button } from '@/components/common/Button/Button';
+
+import s from './styles.module.scss';
+
+type customAccordionProps = {
+  data: {
+    name: string;
+    text: string;
+  }[];
+};
+
+export const CustomAccordion: FC<customAccordionProps> = ({ data }) => {
+  return (
+    <Accordion
+      variant="light"
+      selectionMode="multiple"
+      className={s.accordion}
+      itemClasses={{
+        base: s.base,
+        content: s.content,
+        heading: s.heading,
+        trigger: s.trigger,
+        title: s.title
+      }}
+    >
+      {data.map(({ name, text }, i) => (
+        <AccordionItem
+          key={i}
+          aria-label={`Accordion ${i}`}
+          className={s.accordionItem}
+          title={
+            <Typography variant={'h3'} className={s.itemTitle}>
+              {
+                <span className={s.headerWrapper}>
+                  <span>{name}</span>
+                  <Button
+                    buttonType={'buttonDiv'}
+                    className={s.accordionButton}
+                  >
+                    Mehr lesen
+                  </Button>
+                </span>
+              }
+            </Typography>
+          }
+        >
+          {
+            <Typography variant={'body1'} className={s.itemText}>
+              {text}
+            </Typography>
+          }
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+};
--- src/components/pages/services/FaqSection/FaqSection.tsx
+++ src/components/pages/services/FaqSection/FaqSection.tsx
@@ -1,13 +1,10 @@
-'use client';
-import { Accordion, AccordionItem } from '@heroui/accordion';
-
 import { AppContainer } from '@/components/common/AppContainer/AppContainer';
 import { Typography } from '@/components/common/Typography/Typography';
+import { CustomAccordion } from '@/components/common/CustomAccordion/CustomAccordion';
 
 import faqData from '@/mockData/FaqData.json';
 
 import s from './styles.module.scss';
-import { Button } from '@/components/common/Button/Button';
 
 export const FaqSection = () => {
   return (
@@ -17,48 +14,7 @@ export const FaqSection = () => {
           Häufig gestellte Fragen{' '}
           <Typography variant={'decorSpan'}>(FAQ)</Typography>
         </Typography>
-
-        <Accordion
-          variant="light"
-          selectionMode="multiple"
-          className={s.accordion}
-          itemClasses={{
-            base: s.base,
-            content: s.content,
-            heading: s.heading,
-            trigger: s.trigger,
-            title: s.title
-          }}
-        >
-          {faqData.map(({ name, text }, i) => (
-            <AccordionItem
-              key={i}
-              aria-label={`Accordion ${i}`}
-              className={s.accordionItem}
-              title={
-                <Typography variant={'h3'} className={s.itemTitle}>
-                  {
-                    <span className={s.headerWrapper}>
-                      <span>{name}</span>
-                      <Button
-                        buttonType={'buttonDiv'}
-                        className={s.accordionButton}
-                      >
-                        Mehr lesen
-                      </Button>
-                    </span>
-                  }
-                </Typography>
-              }
-            >
-              {
-                <Typography variant={'body1'} className={s.itemText}>
-                  {text}
-                </Typography>
-              }
-            </AccordionItem>
-          ))}
-        </Accordion>
+        <CustomAccordion data={faqData} />
       </AppContainer>
     </section>
   );
